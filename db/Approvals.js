37611f0e4ab3ee2336819b49e61956dc39b52557
GitPro10 <israfilmiya120@gmail.com>
2023-09-05 03:12:07 +0600

    Minor Fix

--- db/Approvals.js
+++ db/Approvals.js
@@ -35,49 +35,6 @@ const ApprovalSchema = new mongoose.Schema({
     id: {
         type: String,
     },
-
-
-
-    client_code: {
-        type: String,
-    },
-    client_name: {
-        type: String,
-    },
-    folder: {
-        type: String,
-    },
-    quantity: {
-        type: Number,
-    },
-    download_date: {
-        type: String,
-    },
-    delivery_date: {
-        type: String,
-    },
-    delivery_bd_time: {
-        type: String,
-    },
-    task: {
-        type: String,
-    },
-    et: {
-        type: Number,
-    },
-    production: {
-        type: String,
-    },
-    qc1: {
-        type: Number,
-    },
-    comment: {
-        type: String,
-    },
-    status: {
-        type: String,
-    },
-
 }, {
     timestamps: true
 });
