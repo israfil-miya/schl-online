5f96944adb94a425363837636aab0cdf5ea50d66
israfil-miya <israfilmiya120@gmail.com>
2023-09-05 16:16:12 +0600

    Minor Fix

--- components/timecard.js
+++ components/timecard.js
@@ -7,10 +7,10 @@ const TimeCard = ({ city }) => {
     <>
       <div className="card border shadow-sm custom-card">
         <div className="card-header fw-light bg-dark px-2 py-0 text-center custom-card-header">{city.split("/")[1]
-        .replace("_", " ")
-        .replace("Paris", "CET")
-        .replace("Riyadh", "GULF")
-        .replace("Canberra", "Australia")
+          .replace("_", " ")
+          .replace("Paris", "CET")
+          .replace("Riyadh", "GULF")
+          .replace("Canberra", "Australia")
         }</div>
         <div className=" text-center bg-light card-body py-0">
           <span className="fw-medium"><Moment format="hh:mm A" interval={1000} tz={city} /></span>
