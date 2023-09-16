d61ec56b75513f87b35a0fdc913118a71cac316a
israfil-miya <israfilmiya120@gmail.com>
2023-09-16 16:16:23 +0600

    Minor Fix

--- pages/client/[clientId].js
+++ pages/client/[clientId].js
@@ -58,14 +58,14 @@ export default function ClientDetails() {
                     <div className="col rounded-0 card">
                         <div className="card-body">
                             <h5 className="card-title">Client Details</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>
                             <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                     <div className="col rounded-0 card">
                         <div className="card-body">
                             <h5 className="card-title">Create Invoice</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p>
                             <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
