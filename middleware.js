28a1a34b0fe78774722273d1b31e72eba95434eb
israfil-miya <israfilmiya120@gmail.com>
2023-10-27 18:52:14 +0600

    Minor Fix

--- middleware.js
+++ middleware.js
@@ -3,13 +3,13 @@ import { NextResponse } from "next/server";
 const ALLOWED_IPS = process.env.ALLOWEDIP.split(" ");
 
 export default function Middleware(req) {
-  const ip = req.headers["x-forwarded-for"] || req.ip;
+  // const ip = req.headers["x-forwarded-for"] || req.ip;
 
-  if (ip === undefined) return NextResponse.next();
+  // if (ip === undefined) return NextResponse.next();
 
-  if (!ALLOWED_IPS.includes(ip)) {
-    return NextResponse.redirect("https://www.google.com")
-  }
+  // if (!ALLOWED_IPS.includes(ip)) {
+  //   return NextResponse.redirect("https://www.google.com")
+  // }
   
   return NextResponse.next();
 }
