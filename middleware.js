9c6e23b8157974a92a288e281b8215c77d25ce30
israfil-miya <israfilmiya120@gmail.com>
2023-10-27 17:35:01 +0600

    Minor Fix

--- middleware.js
+++ middleware.js
@@ -1,15 +1,16 @@
 import { NextResponse } from "next/server";
 
 export default function Middleware(req) {
-  const allowedIps = ["103.106.236.161"];
-  const ip = req.headers["x-forwarded-for"] || req.ip;
-  // const ip = "103.100.122.100"
+  const allowedIps = process.env.ALLOWEDIP.split(" ")
+  console.log(allowedIps)
+  // const ip = req.headers["x-forwarded-for"] || req.ip;
+  const ip = allowedIps[0]
 
   if (ip === undefined) return NextResponse.next();
 
   if (!allowedIps.includes(ip)) {
     const url = req.nextUrl.clone();
-    if (url.pathname.startsWith("/favicon") || url.pathname.startsWith("/img"))
+    if (url.pathname.startsWith("/favicon") || url.pathname.startsWith("/images"))
       return NextResponse.next();
     return NextResponse.rewrite(new URL("/forbidden", req.url));
   }
