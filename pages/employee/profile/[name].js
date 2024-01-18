713cd77ebfe60a52312c6fae1e30e197babcbdf3
israfil-miya <israfilmiya120@gmail.com>
2024-01-18 21:13:12 +0600

    Minor Fix

--- pages/employee/profile/[name].js
+++ pages/employee/profile/[name].js
@@ -203,7 +203,7 @@ export default function Page() {
                   <div className="row justify-content-between">
                     <div className="col-6 pb-1 px-4 text-start">
                       <p className="m-1 p-0 text-body-secondary fw-semibold">
-                        Employee's Share
+                        Employee&apos;s Share
                       </p>
                       <p className="m-0 p-0">
                         {Math.round(
@@ -215,7 +215,7 @@ export default function Page() {
                     </div>
                     <div className="col-6 pb-1 px-4 text-start">
                       <p className="m-1 p-0 text-body-secondary fw-semibold">
-                        Company's Share
+                        Company&apos;s Share
                       </p>
                       <p className="m-0 p-0">
                         {Math.round(
