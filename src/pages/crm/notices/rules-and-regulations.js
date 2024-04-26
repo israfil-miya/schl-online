f7c1d9e2acc25e9d4b00fd603cd9d4cbdb10a3f2
israfil-miya <israfilmiya120@gmail.com>
2024-04-26 20:22:21 +0600

    linting error resolved

--- src/pages/crm/notices/rules-and-regulations.js
+++ src/pages/crm/notices/rules-and-regulations.js
@@ -14,12 +14,12 @@ function RulesAndRegulations() {
         <ol className='mb-5'>
           <li>Punctuality is key.</li>
           <li>Every employee will undergo 10 tests each month.</li>
-          <li>If an employee does not achieve the target of 10 tests, a deduction of 500 takas will be applied for each test not completed from their monthly commission. If the employee does not have a monthly commission, the deduction will be made from their regular clients' commission, which they will receive after 4 months.</li>
+          <li>If an employee does not achieve the target of 10 tests, a deduction of 500 takas will be applied for each test not completed from their monthly commission. If the employee does not have a monthly commission, the deduction will be made from their regular client&apos;s commission, which they will receive after 4 months.</li>
           <li>A 10% commission will be applicable on amounts exceeding $100.</li>
           <li>Each employee must make calls to 55 individuals daily, find 20 new leads, and follow up with those previously contacted.</li>
-          <li>Marketing employees will receive commissions every 4 months, solely based on regular clients' contributions (irregular or quarterly-based not applicable), such as ITS, WoodWood, THOD.</li>
+          <li>Marketing employees will receive commissions every 4 months, solely based on regular client&apos;s contributions (irregular or quarterly-based not applicable), such as ITS, WoodWood, THOD.</li>
           <li>Monthly incentives will be granted based on performance in 10 tests, and the best seller will be recognized.</li>
-          <li>The royalty commission after 4 months is not set at a fixed percentage; it will be determined based on the company's incurred expenses.</li>
+          <li>The royalty commission after 4 months is not set at a fixed percentage; it will be determined based on the company&apos;s incurred expenses.</li>
         </ol>
 
         <p className='font-monospace'>Please maintain a focused atmosphere in the marketing room by avoiding coffee/tea, games, and loud conversations. Disruptive behaviour may lead to immediate unpaid leave.</p>
