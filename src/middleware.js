793a4284fa6a38633157cd895d3081b1787acbbc
israfil-miya <israfilmiya120@gmail.com>
2024-06-28 19:36:05 +0600

    Middleware bug

--- src/middleware.js
+++ src/middleware.js
@@ -4,12 +4,15 @@ import { getToken } from "next-auth/jwt";
 // This function can be marked `async` if using `await` inside
 export async function middleware(request) {
   const session = await getToken({ req: request, secret: process.env.SECRET });
-  console.log("session in middleware: ", session);
 
-  if (session.user.role == "marketer") {
+  if (session?.user.role == "marketer") {
     return NextResponse.redirect(
       new URL("https://marketers.studioclickhouse.com/")
     );
+  }
+
+  if (session == null) {
+    return NextResponse.next();
   }
 
   return NextResponse.next();
